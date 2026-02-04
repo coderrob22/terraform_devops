7b9557b557e75f90c0a84b862c72df0da164a770
coderrob22 <robertknott7@gmail.com>
2026-02-04 17:02:46 -0500

    terrafor init with gitignore

--- Exercise1/InstID.tf
+++ Exercise1/InstID.tf
@@ -1,6 +1,7 @@
 #We are using a data resource to fetch the data outside of terraform.
 ## Resource type and resource name, followed by reseource block
 
+# Data resource block
 data "aws_ami" "amiID" {
   most_recent = true
 
@@ -15,4 +16,10 @@ data "aws_ami" "amiID" {
   }
 
   owners = ["099720109477"]
+}
+
+# Output resource block
+output "instance_id" {
+  description = "AMI ID of ubuntu instnace"
+  value       = data.aws_ami.amiID.id
 }
